5e39af79403c455c2bb92d67c888bcb0c2239fdb
Rachel Humble <rachelhumble@Rachels-MacBook-Air.local>
2023-11-27 20:53:07 -0500

    adjust file structure

---
+++ client/src/flexpa_types.d.ts
@@ -0,0 +1,10 @@
+export interface LinkExchangeResponse {
+    accessToken: string;
+    expiresIn: number;
+  }
+  
+  export interface FlexpaConfig {
+    publishableKey: string;
+    onSuccess: (publicToken: string) => Promise | unknown;
+  }
+  
